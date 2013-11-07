fed1045e8462841104ae62a1641a4ec914b7b685
Michael Stutz <michaeljstutz@gmail.com>
2013-11-07 09:25:51 -0500

    Added on permalink change add alias functionality

--- core/class-wpp-content-alias-admin.php
+++ core/class-wpp-content-alias-admin.php
@@ -22,6 +22,9 @@ class WPP_Content_Alias_Admin {
 	/** Used to keep the init state of the class */
 	private static $_initialized = false;
 	
+	/** Used to keep the $post_id permalink for comparison */
+	private static $_permalink_compare = array();
+	
 	/**
 	 * 
 	 */
@@ -32,6 +35,34 @@ class WPP_Content_Alias_Admin {
 		wpp_content_alias_init_class( 'WPP_Content_Alias_Admin_Metabox', '/core/class-wpp-content-alias-admin-metabox.php' );
 		wpp_content_alias_init_class( 'WPP_Content_Alias_Admin_Options', '/core/class-wpp-content-alias-admin-options.php' );
 		
+		add_action( 'pre_post_update', array( __CLASS__, 'pre_post_update' ) );
+		add_action( 'save_post', array( __CLASS__, 'save_post' ) );
+		
 		self::$_initialized = true;
+	}
+	
+	/**
+	 * 
+	 */
+	public static function pre_post_update( $post_id ) {
+		if ( wp_is_post_revision( $post_id ) ) //Check to make sure it is not a revision
+			return;
+		
+		self::$_permalink_compare[$post_id] = get_permalink( $post_id );
+	}
+	
+	/**
+	 * 
+	 */
+	public static function save_post( $post_id ) {
+		if ( ! isset( self::$_permalink_compare[$post_id] ) ) //No need to run compare if there is no old value to check against
+			return;
+		
+		$old_permalink = self::$_permalink_compare[$post_id];
+		$new_permalink = get_permalink( $post_id );
+		
+		if ( $old_permalink !== $new_permalink ) {
+			WPP_Content_Alias::add_alias( $post_id, $old_permalink );
+		}
 	}
 }
